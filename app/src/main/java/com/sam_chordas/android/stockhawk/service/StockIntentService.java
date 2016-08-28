94ccea5276d806fa232a4a3e972d613d383ab595
Electron <devversitymail@gmail.com>
2016-08-28 17:53:17 +0530

    Fixed crash on invalid stock symbol

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -3,8 +3,13 @@ package com.sam_chordas.android.stockhawk.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.gcm.TaskParams;
+import com.sam_chordas.android.stockhawk.R;
 
 /**
  * Created by sam_chordas on 10/1/15.
@@ -28,6 +33,20 @@ public class StockIntentService extends IntentService {
     }
     // We can call OnRunTask from the intent service to force it to run immediately instead of
     // scheduling a task.
-    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+    // when user enters invalid symbol, YQL returns null in almost every filed and that throws
+    // exception, see below URL for example JSON response
+    //Valid : https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22MSFT%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+    //inValid : https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22PHUB%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+
+    try {
+      stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+    } catch (NumberFormatException e) {
+      new Handler(Looper.getMainLooper()).post(new Runnable() {
+        @Override
+        public void run() {
+          Toast.makeText(getApplicationContext(), "Invalid SYMBOL for Stock Request", Toast.LENGTH_LONG).show();
+        }
+      });
+    }
   }
 }
