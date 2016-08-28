84fe7a46762d34031ed4f8922047e09ffc8c7c8d
Electron <devversitymail@gmail.com>
2016-08-28 23:21:17 +0530

    Added action bar in details activity

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailGraphActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailGraphActivity.java
@@ -1,19 +1,18 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.sam_chordas.android.stockhawk.R;
 
 
-public class DetailGraphActivity extends Activity {
+public class DetailGraphActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_graph);
-//        causes crash, fix this
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
