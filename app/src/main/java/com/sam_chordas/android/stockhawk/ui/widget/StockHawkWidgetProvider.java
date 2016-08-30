3a48704f3d2e80ea02400914c7e1c4f29d938654
Electron <devversitymail@gmail.com>
2016-08-30 08:25:39 +0530

    Finished Widget

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/widget/StockHawkWidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/widget/StockHawkWidgetProvider.java
@@ -1,46 +1,111 @@
 package com.sam_chordas.android.stockhawk.ui.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.sam_chordas.android.stockhawk.ui.DetailGraphActivity;
 
 
 /**
  * Implementation of App Widget functionality.
  */
 public class StockHawkWidgetProvider extends AppWidgetProvider {
+    public static String CLICK_ACTION = "com.sam_chordas.android.quotelistwidget.CLICK";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    private static HandlerThread sWorkerThread;
+    private static Handler sWorkerQueue;
+    private static QuoteDataProviderObserver sDataObserver;
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_hawk_widget);
-//        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+    public StockHawkWidgetProvider() {
+        sWorkerThread = new HandlerThread("QuoteWidgetProvider-worker");
+        sWorkerThread.start();
+        sWorkerQueue = new Handler(sWorkerThread.getLooper());
     }
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+        final ContentResolver r = context.getContentResolver();
+        if (sDataObserver == null) {
+            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+            final ComponentName cn = new ComponentName(context, StockHawkWidgetProvider.class);
+            sDataObserver = new QuoteDataProviderObserver(mgr, cn, sWorkerQueue);
+            r.registerContentObserver(QuoteProvider.Quotes.CONTENT_URI, true, sDataObserver);
+        }
     }
 
     @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+    public void onReceive(Context ctx, Intent intent) {
+        final String action = intent.getAction();
+        if (action.equals(CLICK_ACTION)) {
+            final String symbol = intent.getStringExtra(DetailGraphActivity.SELECTED_SYMBOL);
+
+            Intent i = new Intent(ctx, DetailGraphActivity.class);
+            i.setFlags (Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.putExtra(DetailGraphActivity.SELECTED_SYMBOL, symbol);
+            ctx.startActivity(i);
+        }
+        super.onReceive(ctx, intent);
+    }
+
+    private RemoteViews buildLayout(Context context, int appWidgetId) {
+        RemoteViews rv;
+
+        final Intent intent = new Intent(context, StockHawkWidgetRemoteViewService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        rv = new RemoteViews(context.getPackageName(), R.layout.stock_hawk_widget);
+        rv.setRemoteAdapter(R.id.widget_list, intent);
+
+        final Intent onClickIntent = new Intent(context, StockHawkWidgetProvider.class);
+        onClickIntent.setAction(StockHawkWidgetProvider.CLICK_ACTION);
+        onClickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        onClickIntent.setData(Uri.parse(onClickIntent.toUri(Intent.URI_INTENT_SCHEME)));
+        final PendingIntent onClickPendingIntent = PendingIntent.getBroadcast(context, 0,
+                onClickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        rv.setPendingIntentTemplate(R.id.widget_list, onClickPendingIntent);
+
+        return rv;
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Update each of the widgets with the remote adapter
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            RemoteViews layout = buildLayout(context, appWidgetIds[i]);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], layout);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
+
+class QuoteDataProviderObserver extends ContentObserver {
+    private AppWidgetManager mAppWidgetManager;
+    private ComponentName mComponentName;
+
+    QuoteDataProviderObserver(AppWidgetManager mgr, ComponentName cn, Handler h) {
+        super(h);
+        mAppWidgetManager = mgr;
+        mComponentName = cn;
+    }
+    @Override
+    public void onChange(boolean selfChange) {
+        // The data has changed, so notify the widget that the collection view needs to be updated.
+        // In response, the factory's onDataSetChanged() will be called which will requery the
+        // cursor for the new data.
+        mAppWidgetManager.notifyAppWidgetViewDataChanged(
+                mAppWidgetManager.getAppWidgetIds(mComponentName), R.id.widget_list);
     }
 }
 
